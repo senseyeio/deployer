56e713ec6afabd5a4821a882ad278df0dad30749
Mike Jewell <mjewell@gmail.com>
2018-06-25 15:40:20 +0100

    Testing with logging in per script

--- deploy_image.sh
+++ deploy_image.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euv pipefail
 
+aws configure set region ${AWS_REGION}
+dockerLoginCmd=$(aws ecr get-login --no-include-email)
+echo $($dockerLoginCmd)
+
 sed -i -e "s#build: .#image: ${AWS_ACCOUNT}.dkr.ecr.eu-west-1.amazonaws.com/prognosys/${SERVICE_NAME}:${BUILD_NO}#g" docker-compose.yml
 ecs-cli configure --cluster ${CLUSTER_NAME} --region ${AWS_REGION}
 ecs-cli compose -p ${SERVICE_NAME} service up
